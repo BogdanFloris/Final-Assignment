8104ed9e7a194e4811908393d3c89713838f9256
Bogdan Floris <bogdanfloris@MBP.local>
2016-05-31 16:32:02 +0200

    Homepage v2

--- jscode.js
+++ jscode.js
@@ -1,13 +1,1 @@
-$( "#spinner" ).spinner();
-$(function() {
-    $( "#spinner" ).spinner({
-      step: 0.1,
-      numberFormat: "n"
-    });
 
-    $( "#culture" ).change(function() {
-      var current = $( "#spinner" ).spinner( "value" );
-      Globalize.culture( $(this).val() );
-      $( "#spinner" ).spinner( "value", current );
-    });
-  });
